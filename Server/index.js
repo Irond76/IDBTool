86843c0bb24156b784d17be121364c730c391ff8
Dale Bendixen <dalebendixen@gmail.com>
2023-12-12 15:01:35 -0500

    moved server into project

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors()); 
+
+app.get('/', (req, res) => {
+    res.json({msg: "hello from express"})
+});
+
+
+
+app.listen(3000, () => {
+    console.log('server started on 3000...')
+})
